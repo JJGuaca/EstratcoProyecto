0a69ce424bd9b6c262f73c6bd6bf5f384ec33de0
JJGuaca <57268596+JJGuaca@users.noreply.github.com>
2020-07-16 16:02:15 -0500

    Scale
    Accion de boton red

--- js/propios.js
+++ js/propios.js
@@ -1,3 +1,8 @@
-document.ready(function(){
-	alert('Cargando');
-});
+function scale(){
+	var btn = document.getElementById('redpro');
+	btn.style.setProperty('transform','scale(1.1)'); 
+}
+function scalenot(){
+	var btn = document.getElementById('redpro');
+	btn.style.setProperty('transform','scale(1)'); 
+}
