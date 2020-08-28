9449cf71c1d4508d5edb152e56323ffd5d2a8720
JJGuaca <57268596+JJGuaca@users.noreply.github.com>
2020-08-28 16:02:40 -0500

    Ajuste
    JScrip dado que no usa php

--- js/propios.js
+++ js/propios.js
@@ -8,19 +8,31 @@ function scalenot(btn){
 }
 
 $(document).ready(function(){
+	var nbar = document.getElementById('nbar');
+	var nbarjs = new XMLHttpRequest();
+	nbarjs.open('GET','./vistas/navbar.html',false);
+	nbarjs.send();
+	nbar.innerHTML =nbarjs.responseText;
+	var fter = document.getElementById('fter');
+	var fterjs = new XMLHttpRequest();
+	fterjs.open('GET','./vistas/footer.html',false);
+	fterjs.send();
+	fter.innerHTML=fterjs.responseText;
+
 	var sitio = document.getElementById('SITIO');
 	var titulo = document.getElementById('titulo').innerText; 
 
 if(titulo == 'Nosotros'){
 	sitio.innerText='Nosotros';
 	$('#link1').attr('href','.');
-	$('#link2').attr('href','nosotros.php');
-	$('#link4').attr('href','educacion.php');
+	$('#link2').attr('href','nosotros.html');
+	$('#link4').attr('href','educacion.html');
+
 }else if(titulo == 'Educación'){
 	sitio.innerText='Educación';
 	$('#link1').attr('href','.');
-	$('#link2').attr('href','nosotros.php');
-	$('#link4').attr('href','educacion.php');
+	$('#link2').attr('href','nosotros.html');
+	$('#link4').attr('href','educacion.html');
 
 }else{
 	console.log('no igual');
